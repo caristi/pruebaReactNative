c21c27e58e6d3b147c7ef29908344e47c55cc841
Carlos Aristizabal <carloshugo5@gmail.com>
2019-07-28 16:47:40 -0500

    Prueba para Abrir URL en el navegador y consumir servicio rest

--- src/screens/containers/evento.js
+++ src/screens/containers/evento.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import{View, Text, Button}from 'react-native';
+import{View, Text, Button,WebView,Linking}from 'react-native';
 
 class Evento extends Component{
 
@@ -8,12 +8,20 @@ class Evento extends Component{
   }
 
       render() {
+
+        const uri = 'http://sanluisgonzaga.edu.co/';
+
         return (
           <View>
             <Text>Estoy en la pantalla de Evento 10</Text>
             <Button title="ir a Login"
                onPress={this.handlePress}/>
+
+              <Button title='Ir a Web de Colegio' onPress={ ()=> Linking.openURL(uri) } />
+
           </View>
+          
+          
         );
       }
 }
--- src/screens/containers/inicio.js
+++ src/screens/containers/inicio.js
@@ -3,20 +3,69 @@ import{
     View,
     Text,
     Button,
+    FlatList,
 }from 'react-native';
 
 class Inicio extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      loading:false,
+      pokemon:[],
+      url: 'https://pokeapi.co/api/v2/pokemon/'
+    }
+  }
+
+  componentDidMount(){
+    this.getPokemon();
+  }
+
+  getPokemon = () =>{
+    this.setState({loading:true})
+
+    fetch(this.state.url)
+    .then(res => res.json())
+    .then(res => {
+
+        this.setState({
+          pokemon : res.results,
+            url: res.next,
+            loading:false
+        })
+    });
+  }
 
   handlePress = () => {
     this.props.navigation.navigate('Evento')
   }
 
       render() {
+
+        if(this.state.loading){
+
+          return (
+            <View>
+              <Text> DESCARGANDO CONEXION A API </Text>
+            </View>
+          );
+
+        }
+
         return (
           <View>
             <Text>Estoy en la pagina de Inicio con su nombre {this.props.navigation.getParam('text')} </Text>
             <Button title="ir a evento"
-            onPress={this.handlePress}/>
+                    onPress={this.handlePress}/>
+            <Text>--</Text>        
+            <Text>Cargada datos de API</Text>
+
+            <FlatList 
+              data={this.state.pokemon}
+              renderItem={
+                ({item}) => <Text>{item.name}</Text>
+              }
+            />
           </View>
         );
       }
